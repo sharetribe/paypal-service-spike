jahde/sharetribe

Get shipping address from PP and save

app/services/paypal_service/api/payments.rb
@@ -258,11 +258,12 @@ module PaypalService::API
             finally: method(:handle_failed_create_payment).call(token),
           }
         ) do |ec_details|
-
           # Validate that the buyer accepted and we have a payer_id now
           if (ec_details[:payer_id].nil?)
             return Result::Error.new("Payment has not been accepted by the buyer.")
           end
+
+          @events.send(:details_received, :success, ec_details.merge(transaction_id: token[:transaction_id]))
 
           with_success(token[:community_id], token[:transaction_id],
             MerchantData.create_do_express_checkout_payment({

app/services/paypal_service/paypal_service_injector.rb
@@ -41,6 +41,9 @@ module PaypalService
           request_cancelled: -> (flow, token) {
             TransactionService::PaypalEvents.request_cancelled(flow, token)
           },
+          details_received: -> (flow, details) {
+            TransactionService::PaypalEvents.details_received(flow, details)
+          },
           payment_created: [],
           payment_updated: -> (flow, payment) { TransactionService::PaypalEvents.payment_updated(flow, payment) }
       })

sharetribe/paypal-service-spike

api/payments.rb
@@ -258,11 +258,12 @@ module PaypalService::API
             finally: method(:handle_failed_create_payment).call(token),
           }
         ) do |ec_details|
-
           # Validate that the buyer accepted and we have a payer_id now
           if (ec_details[:payer_id].nil?)
             return Result::Error.new("Payment has not been accepted by the buyer.")
           end
+
+          @events.send(:order_details, :success, ec_details.merge(transaction_id: token[:transaction_id]))
 
           with_success(token[:community_id], token[:transaction_id],
             MerchantData.create_do_express_checkout_payment({

paypal_service_injector.rb
@@ -41,6 +41,9 @@ module PaypalService
           request_cancelled: -> (flow, token) {
             TransactionService::PaypalEvents.request_cancelled(flow, token)
           },
+          order_details: -> (flow, details) {
+            TransactionService::PaypalEvents.update_transaction_details(flow, details)
+          },
           payment_created: [],
           payment_updated: -> (flow, payment) { TransactionService::PaypalEvents.payment_updated(flow, payment) }
       })